1327d545ec36735f9ad33f1803e8081a3f23c4f1
Dr. Massimiliano Pala <madwolf@openca.org>
2024-10-16 18:26:43 -0400

    Added initial skeleton for PK utility for generating crypto artifacts (keys, CSRs, Certs).

---
+++ examples/pktool/pktool.h
@@ -0,0 +1,11 @@
+/* wc_pktool.h */
+
+#include <wolfssl/options.h>
+#include <wolfssl/wolfcrypt/settings.h>
+#include <wolfssl/wolfcrypt/ecc.h>
+#include <wolfssl/wolfcrypt/rsa.h>
+#include <wolfssl/wolfcrypt/ed25519.h>
+#include <wolfssl/wolfcrypt/asn_public.h>
+#include <wolfssl/wolfcrypt/error-crypt.h>
+
+#define LARGE_TEMP_SZ 4096
