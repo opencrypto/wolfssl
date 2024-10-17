392d40132aa0dd07d79635f48a8343249bbf2e84
Dr. Massimiliano Pala <madwolf@openca.org>
2024-10-17 17:07:28 -0400

    Added support for '-out' switch in pktool's example.

--- examples/pktool/pktool.c
+++ examples/pktool/pktool.c
@@ -1,57 +1,265 @@
-/* csr_example.c */
+/* pktool.c */
 
 #include "pktool.h"
 
-#define LARGE_TEMP_SZ 4096
-
-static void usage(void)
-{
+static void usage(void) {
     printf("Invalid input supplied try one of the below examples\n");
-    printf("Examples:\n\n");
-    printf("./csr_example rsa\n");
-    printf("./csr_example ecc\n");
-    printf("./csr_example ed25519\n");
+    printf("\n    USAGE: pktool [ options ]\n\n");
+    printf("Where [ options ] are:\n");
+    printf(" -v ...............: verbose\n");
+    printf(" -d ...............: debug\n");
+    printf(" -algor <name> ....: use the named algorithm (e.g., rsa, mldsa44, mldsa65-ed25519\n");
+    printf("\n");
+    exit(1);
 }
 
-static int gen_csr(const char* arg1)
-{
+// static int gen_csr(const char* arg1)
+// {
+//     int ret;
+//     int type;
+// #ifdef HAVE_ECC
+//     ecc_key ecKey;
+// #endif
+// #ifndef NO_RSA
+//     RsaKey rsaKey;
+// #endif
+// #ifdef HAVE_ED25519
+//     ed25519_key edKey;
+// #endif
+//     void* keyPtr = NULL;
+//     WC_RNG rng;
+//     Cert req;
+//     byte der[LARGE_TEMP_SZ];
+//     int  derSz;
+// #ifdef WOLFSSL_DER_TO_PEM
+//     byte pem[LARGE_TEMP_SZ];
+//     int  pemSz;
+//     FILE* file = NULL;
+//     char outFile[255];
+// #endif
+
+//     XMEMSET(der, 0, LARGE_TEMP_SZ);
+// #ifdef WOLFSSL_DER_TO_PEM
+//     XMEMSET(pem, 0, LARGE_TEMP_SZ);
+// #endif
+
+//     if (XSTRNCMP(arg1, "rsa", 3) == 0)
+//         type = RSA_TYPE;
+//     else if (XSTRNCMP(arg1, "ecc", 3) == 0)
+//         type = ECC_TYPE;
+//     else if (XSTRNCMP(arg1, "ed25519", 7) == 0)
+//         type = ED25519_TYPE;
+//     else
+//         return NOT_COMPILED_IN;
+
+    
+//     ret = wc_InitRng(&rng);
+//     if (ret != 0) {
+//         printf("RNG initialization failed: %d\n", ret);
+//         return ret;
+//     }
+
+// #ifdef HAVE_ECC
+//     if (type == ECC_TYPE) {
+//         keyPtr = &ecKey;
+//         ret = wc_ecc_init(&ecKey);
+//     }
+// #endif
+// #ifndef NO_RSA
+//     if (type == RSA_TYPE) {
+//         keyPtr = &rsaKey;
+//         ret = wc_InitRsaKey(&rsaKey, NULL);
+//     }
+// #endif
+// #ifdef HAVE_ED25519
+//     if (type == ED25519_TYPE) {
+//         keyPtr = &edKey;
+//         ret = wc_ed25519_init(&edKey);
+//     }
+// #endif
+//     if (ret != 0) {
+//         printf("Key initialization failed: %d\n", ret);
+//         goto exit;
+//     }
+
+// #ifdef HAVE_ECC
+//     if (type == ECC_TYPE)
+//         ret = wc_ecc_make_key_ex(&rng, 32, &ecKey, ECC_SECP256R1);
+// #endif
+// #ifndef NO_RSA
+//     if (type == RSA_TYPE)
+//         ret = wc_MakeRsaKey(&rsaKey, 2048, WC_RSA_EXPONENT, &rng);
+// #endif
+// #ifdef HAVE_ED25519
+//     if (type == ED25519_TYPE)
+//         ret = wc_ed25519_make_key(&rng, ED25519_KEY_SIZE, &edKey);
+// #endif
+//     if (ret != 0) {
+//         printf("Key generation failed: %d\n", ret);
+//         goto exit;
+//     }
+
+// #ifdef HAVE_ECC
+//     if (type == ECC_TYPE)
+//         ret = wc_EccKeyToDer(&ecKey, der, sizeof(der));
+// #endif
+// #ifndef NO_RSA
+//     if (type == RSA_TYPE)
+//         ret = wc_RsaKeyToDer(&rsaKey, der, sizeof(der));
+// #endif
+// #ifdef HAVE_ED25519
+//     if (type == ED25519_TYPE)
+//         ret = wc_Ed25519KeyToDer(&edKey, der, sizeof(der));
+// #endif
+//     if (ret <= 0) {
+//         printf("Key To DER failed: %d\n", ret);
+//         goto exit;
+//     }
+//     derSz = ret;
+
+// #ifdef WOLFSSL_DER_TO_PEM
+//     memset(pem, 0, sizeof(pem));
+// #ifdef HAVE_ECC
+//     if (type == ECC_TYPE)
+//         ret = wc_DerToPem(der, derSz, pem, sizeof(pem), ECC_PRIVATEKEY_TYPE);
+// #endif
+// #ifndef NO_RSA
+//     if (type == RSA_TYPE)
+//         ret = wc_DerToPem(der, derSz, pem, sizeof(pem), PRIVATEKEY_TYPE);
+// #endif
+// #ifdef HAVE_ED25519
+//     if (type == ED25519_TYPE)
+//         ret = wc_DerToPem(der, derSz, pem, sizeof(pem), ED25519_TYPE);
+// #endif
+//     if (ret <= 0) {
+//         printf("Key DER to PEM failed: %d\n", ret);
+//         goto exit;
+//     }
+//     pemSz = ret;
+//     printf("%s (%d)\n", pem, pemSz);
+
+//     snprintf(outFile, sizeof(outFile), "%s-key.pem", arg1);
+//     printf("Saved Key PEM to \"%s\"\n", outFile);
+//     file = fopen(outFile, "wb");
+//     if (file) {
+//         ret = (int)fwrite(pem, 1, pemSz, file);
+//         fclose(file);
+//     }
+// #endif /* WOLFSSL_DER_TO_PEM */
+
+//     ret = wc_InitCert(&req);
+//     if (ret != 0) {
+//         printf("Init Cert failed: %d\n", ret);
+//         goto exit;
+//     }
+//     strncpy(req.subject.country, "US", CTC_NAME_SIZE);
+//     strncpy(req.subject.state, "OR", CTC_NAME_SIZE);
+//     strncpy(req.subject.locality, "Portland", CTC_NAME_SIZE);
+//     strncpy(req.subject.org, "wolfSSL", CTC_NAME_SIZE);
+//     strncpy(req.subject.unit, "Development", CTC_NAME_SIZE);
+//     strncpy(req.subject.commonName, "www.wolfssl.com", CTC_NAME_SIZE);
+//     strncpy(req.subject.email, "info@wolfssl.com", CTC_NAME_SIZE);
+//     req.version = 0;
+//     ret = wc_MakeCertReq_ex(&req, der, sizeof(der), type, keyPtr);
+//     if (ret <= 0) {
+//         printf("Make Cert Req failed: %d\n", ret);
+//         goto exit;
+//     }
+//     derSz = ret;
+
+// #ifdef HAVE_ECC
+//     if (type == ECC_TYPE)
+//         req.sigType = CTC_SHA256wECDSA;
+// #endif
+// #ifndef NO_RSA
+//     if (type == RSA_TYPE)
+//         req.sigType = CTC_SHA256wRSA;
+// #endif
+// #ifdef HAVE_ED25519
+//     if (type == ED25519_TYPE)
+//         req.sigType = CTC_ED25519;
+// #endif
+//     ret = wc_SignCert_ex(req.bodySz, req.sigType, der, sizeof(der), type,
+//         keyPtr, &rng);
+//     if (ret <= 0) {
+//         printf("Sign Cert failed: %d\n", ret);
+//         goto exit;
+//     }
+//     derSz = ret;
+
+// #ifdef WOLFSSL_DER_TO_PEM
+//     memset(pem, 0, sizeof(pem));
+//     ret = wc_DerToPem(der, derSz, pem, sizeof(pem), CERTREQ_TYPE);
+//     if (ret <= 0) {
+//         printf("CSR DER to PEM failed: %d\n", ret);
+//         goto exit;
+//     }
+//     pemSz = ret;
+//     printf("%s (%d)\n", pem, pemSz);
+
+//     snprintf(outFile, sizeof(outFile), "%s-csr.pem", arg1);
+//     printf("Saved CSR PEM to \"%s\"\n", outFile);
+//     file = fopen(outFile, "wb");
+//     if (file) {
+//         ret = (int)fwrite(pem, 1, pemSz, file);
+//         fclose(file);
+//     }
+// #endif
+
+//     ret = 0; /* success */
+    
+// exit:
+// #ifdef HAVE_ECC
+//     if (type == ECC_TYPE)
+//         wc_ecc_free(&ecKey);
+// #endif
+// #ifndef NO_RSA
+//     if (type == RSA_TYPE)
+//         wc_FreeRsaKey(&rsaKey);
+// #endif
+// #ifdef HAVE_ED25519
+//     if (type == ED25519_TYPE)
+//         wc_ed25519_free(&edKey);
+// #endif
+//     wc_FreeRng(&rng);
+
+//     return ret;
+// }
+
+static int gen_keypair(int type, const char * out) {
+
+    if (type < 0 || !out) {
+        printf("Invalid key type\n");
+        return 1;
+    }
+
     int ret;
-    int type;
-#ifdef HAVE_ECC
-    ecc_key ecKey;
-#endif
 #ifndef NO_RSA
     RsaKey rsaKey;
 #endif
+#ifdef HAVE_DSA
+    dsa_key dsaKey;
+#endif
+#ifdef HAVE_ECC
+    ecc_key ecKey;
+#endif
 #ifdef HAVE_ED25519
-    ed25519_key edKey;
+    ed25519_key ed25519Key;
+#endif
+#ifdef HAVE_ED448
+    ed448_key ed448Key;
+#endif
+#ifdef HAVE_DILITHIUM
+    dilithium_key dilithiumKey;
+#endif
+#ifdef HAVE_FALCON
+    falcon_key falconKey;
+#endif
+#ifdef HAVE_MLDSA_COMPOSITE
+    mldsa_composite_key mldsa_compositeKey;
 #endif
     void* keyPtr = NULL;
     WC_RNG rng;
-    Cert req;
-    byte der[LARGE_TEMP_SZ];
-    int  derSz;
-#ifdef WOLFSSL_DER_TO_PEM
-    byte pem[LARGE_TEMP_SZ];
-    int  pemSz;
-    FILE* file = NULL;
-    char outFile[255];
-#endif
-
-    XMEMSET(der, 0, LARGE_TEMP_SZ);
-#ifdef WOLFSSL_DER_TO_PEM
-    XMEMSET(pem, 0, LARGE_TEMP_SZ);
-#endif
-
-    if (XSTRNCMP(arg1, "rsa", 3) == 0)
-        type = RSA_TYPE;
-    else if (XSTRNCMP(arg1, "ecc", 3) == 0)
-        type = ECC_TYPE;
-    else if (XSTRNCMP(arg1, "ed25519", 7) == 0)
-        type = ED25519_TYPE;
-    else
-        return NOT_COMPILED_IN;
-
     
     ret = wc_InitRng(&rng);
     if (ret != 0) {
@@ -59,171 +267,48 @@ static int gen_csr(const char* arg1)
         return ret;
     }
 
-#ifdef HAVE_ECC
-    if (type == ECC_TYPE) {
-        keyPtr = &ecKey;
-        ret = wc_ecc_init(&ecKey);
-    }
+    switch (type) {
+#ifdef HAVE_DSA
+    case DSAk:
+        keyPtr = &dsaKey;
+        ret = wc_InitDsaKey(&dsaKey, NULL);
+        break;
 #endif
 #ifndef NO_RSA
-    if (type == RSA_TYPE) {
+    case RSAk:
         keyPtr = &rsaKey;
         ret = wc_InitRsaKey(&rsaKey, NULL);
-    }
+        if (ret == 0)
+            ret = wc_MakeRsaKey(&rsaKey, 2048, WC_RSA_EXPONENT, &rng);
+        break;
 #endif
-#ifdef HAVE_ED25519
-    if (type == ED25519_TYPE) {
-        keyPtr = &edKey;
-        ret = wc_ed25519_init(&edKey);
-    }
-#endif
-    if (ret != 0) {
-        printf("Key initialization failed: %d\n", ret);
-        goto exit;
-    }
-
 #ifdef HAVE_ECC
-    if (type == ECC_TYPE)
-        ret = wc_ecc_make_key_ex(&rng, 32, &ecKey, ECC_SECP256R1);
-#endif
-#ifndef NO_RSA
-    if (type == RSA_TYPE)
-        ret = wc_MakeRsaKey(&rsaKey, 2048, WC_RSA_EXPONENT, &rng);
-#endif
-#ifdef HAVE_ED25519
-    if (type == ED25519_TYPE)
-        ret = wc_ed25519_make_key(&rng, ED25519_KEY_SIZE, &edKey);
-#endif
-    if (ret != 0) {
-        printf("Key generation failed: %d\n", ret);
-        goto exit;
-    }
-
-#ifdef HAVE_ECC
-    if (type == ECC_TYPE)
-        ret = wc_EccKeyToDer(&ecKey, der, sizeof(der));
-#endif
-#ifndef NO_RSA
-    if (type == RSA_TYPE)
-        ret = wc_RsaKeyToDer(&rsaKey, der, sizeof(der));
+    case ECDSAk:
+        keyPtr = &ecKey;
+        ret = wc_ecc_init(&ecKey);
+        if (ret == 0)
+            ret = wc_ecc_make_key_ex(&rng, 32, keyPtr, ECC_SECP256R1);
+        break;
 #endif
 #ifdef HAVE_ED25519
-    if (type == ED25519_TYPE)
-        ret = wc_Ed25519KeyToDer(&edKey, der, sizeof(der));
-#endif
-    if (ret <= 0) {
-        printf("Key To DER failed: %d\n", ret);
-        goto exit;
-    }
-    derSz = ret;
-
-#ifdef WOLFSSL_DER_TO_PEM
-    memset(pem, 0, sizeof(pem));
-#ifdef HAVE_ECC
-    if (type == ECC_TYPE)
-        ret = wc_DerToPem(der, derSz, pem, sizeof(pem), ECC_PRIVATEKEY_TYPE);
-#endif
-#ifndef NO_RSA
-    if (type == RSA_TYPE)
-        ret = wc_DerToPem(der, derSz, pem, sizeof(pem), PRIVATEKEY_TYPE);
-#endif
-#ifdef HAVE_ED25519
-    if (type == ED25519_TYPE)
-        ret = wc_DerToPem(der, derSz, pem, sizeof(pem), ED25519_TYPE);
-#endif
-    if (ret <= 0) {
-        printf("Key DER to PEM failed: %d\n", ret);
-        goto exit;
-    }
-    pemSz = ret;
-    printf("%s (%d)\n", pem, pemSz);
-
-    snprintf(outFile, sizeof(outFile), "%s-key.pem", arg1);
-    printf("Saved Key PEM to \"%s\"\n", outFile);
-    file = fopen(outFile, "wb");
-    if (file) {
-        ret = (int)fwrite(pem, 1, pemSz, file);
-        fclose(file);
-    }
-#endif /* WOLFSSL_DER_TO_PEM */
-
-    ret = wc_InitCert(&req);
-    if (ret != 0) {
-        printf("Init Cert failed: %d\n", ret);
-        goto exit;
-    }
-    strncpy(req.subject.country, "US", CTC_NAME_SIZE);
-    strncpy(req.subject.state, "OR", CTC_NAME_SIZE);
-    strncpy(req.subject.locality, "Portland", CTC_NAME_SIZE);
-    strncpy(req.subject.org, "wolfSSL", CTC_NAME_SIZE);
-    strncpy(req.subject.unit, "Development", CTC_NAME_SIZE);
-    strncpy(req.subject.commonName, "www.wolfssl.com", CTC_NAME_SIZE);
-    strncpy(req.subject.email, "info@wolfssl.com", CTC_NAME_SIZE);
-    req.version = 0;
-    ret = wc_MakeCertReq_ex(&req, der, sizeof(der), type, keyPtr);
-    if (ret <= 0) {
-        printf("Make Cert Req failed: %d\n", ret);
-        goto exit;
-    }
-    derSz = ret;
-
-#ifdef HAVE_ECC
-    if (type == ECC_TYPE)
-        req.sigType = CTC_SHA256wECDSA;
-#endif
-#ifndef NO_RSA
-    if (type == RSA_TYPE)
-        req.sigType = CTC_SHA256wRSA;
-#endif
-#ifdef HAVE_ED25519
-    if (type == ED25519_TYPE)
-        req.sigType = CTC_ED25519;
-#endif
-    ret = wc_SignCert_ex(req.bodySz, req.sigType, der, sizeof(der), type,
-        keyPtr, &rng);
-    if (ret <= 0) {
-        printf("Sign Cert failed: %d\n", ret);
-        goto exit;
-    }
-    derSz = ret;
-
-#ifdef WOLFSSL_DER_TO_PEM
-    memset(pem, 0, sizeof(pem));
-    ret = wc_DerToPem(der, derSz, pem, sizeof(pem), CERTREQ_TYPE);
-    if (ret <= 0) {
-        printf("CSR DER to PEM failed: %d\n", ret);
-        goto exit;
-    }
-    pemSz = ret;
-    printf("%s (%d)\n", pem, pemSz);
-
-    snprintf(outFile, sizeof(outFile), "%s-csr.pem", arg1);
-    printf("Saved CSR PEM to \"%s\"\n", outFile);
-    file = fopen(outFile, "wb");
-    if (file) {
-        ret = (int)fwrite(pem, 1, pemSz, file);
-        fclose(file);
-    }
+    case ED25519k:
+        keyPtr = &ed25519Key;
+        ret = wc_ed25519_init(&ed25519Key);
+        if (ret == 0)
+            ret = wc_ed25519_make_key(&rng, 32, keyPtr);
+        break;
 #endif
 
-    ret = 0; /* success */
-    
-exit:
-#ifdef HAVE_ECC
-    if (type == ECC_TYPE)
-        wc_ecc_free(&ecKey);
-#endif
-#ifndef NO_RSA
-    if (type == RSA_TYPE)
-        wc_FreeRsaKey(&rsaKey);
-#endif
-#ifdef HAVE_ED25519
-    if (type == ED25519_TYPE)
-        wc_ed25519_free(&edKey);
-#endif
-    wc_FreeRng(&rng);
+        default:
+            return BAD_FUNC_ARG;
+    }
 
-    return ret;
+    (void)keyPtr;
+    (void)mldsa_compositeKey;
+    (void)dilithiumKey;
+    (void)ed448Key;
+
+    return 0;
 }
 
 int main(int argc, char** argv)
@@ -232,9 +317,10 @@ int main(int argc, char** argv)
     printf("Please compile wolfSSL with --enable-certreq --enable-certgen --enable-certext --enable-keygen\n");
     return 0;
 #else
-    enum Key_Sum keySum = RSAk;
+    enum Key_Sum keySum = ML_DSA_LEVEL2k;
     int verbose = 0;
     int debug = 0;
+    const char * out = "key.der";
     int i = 1;
 
     while (i < argc) {
@@ -252,17 +338,19 @@ int main(int argc, char** argv)
                 return 1;
             }
             printf("Algorithm type: %d (%s)\n", keySum, wc_KeySum_name(keySum));
+        } else if (XSTRNCMP(argv[i++], "-out", 4) == 0) {
+            out = argv[i];
+        } else {
+            printf("\n     ERROR: option \"%s\" was not recognized.\n\n", argv[i]);
+            break;
         }
         i++;
     }
 
-    if (argc != 2) {
-        if (verbose) usage();
-        return 1;
-    }
+    return gen_keypair(keySum, out);
 
     (void)debug;
+    (void)verbose;
 
-    return gen_csr(argv[1]);
 #endif
 }
--- examples/pktool/pktool.h
+++ examples/pktool/pktool.h
@@ -4,7 +4,12 @@
 #include <wolfssl/wolfcrypt/settings.h>
 #include <wolfssl/wolfcrypt/ecc.h>
 #include <wolfssl/wolfcrypt/rsa.h>
+#include <wolfssl/wolfcrypt/dsa.h>
+#include <wolfssl/wolfcrypt/dilithium.h>
+#include <wolfssl/wolfcrypt/falcon.h>
 #include <wolfssl/wolfcrypt/ed25519.h>
+#include <wolfssl/wolfcrypt/ed448.h>
+#include <wolfssl/wolfcrypt/mldsa_composite.h>
 #include <wolfssl/wolfcrypt/asn_public.h>
 #include <wolfssl/wolfcrypt/error-crypt.h>
 #include <wolfssl/wolfcrypt/asn.h>
